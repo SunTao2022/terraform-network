8a5e8688d77a2283b8b865dff38079229a2114f0
tao sun <suntaoca2022@gmail.com>
2026-08-10 13:55:56 -0300

    terraform-network: VNet + subnets + NSG with module

---
+++ modules/network/variables.tf
@@ -0,0 +1,20 @@
+variable "vnet_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "web_cidr" {
+  type = string
+}
+
+variable "db_cidr" {
+  type = string
+}
+
