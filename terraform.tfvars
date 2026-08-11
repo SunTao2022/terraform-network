223efdda27202f871c09bddf7917750d2045f837
tao sun <suntaoca2022@gmail.com>
2026-08-11 19:32:07 -0300

    updata non-sensitive-tfvars to git

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+vnet_name           = "vnet-demo"
+location            = "canadacentral"
+resource_group_name = "rg-network-demo"
+web_cidr            = "10.0.1.0/24"
+db_cidr             = "10.0.2.0/24"
+
--- variables.tf
+++ variables.tf
@@ -15,11 +15,13 @@ variable "resource_group_name" {
 
 variable "web_cidr" {
   description = "web_cidr"
+  default = "10.0.1.0/24"
   type        = string
 }
 
 variable "db_cidr" {
-  description = "dc_cidr"
+  description = "db_cidr"
+  default = "10.0.2.0/24"
   type        = string
 }
 
